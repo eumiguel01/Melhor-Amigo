0e2bc284c074e27ac9d9462b372cdb6ee49f4833
Miguel da Silva Xavier <143849967+eumiguel01@users.noreply.github.com>
2024-05-27 14:57:41 -0300

    Criei a página de produtos e excluir a parte de navegação entre abas do js

--- min.js
+++ min.js
@@ -1,26 +1,3 @@
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].classList.remove("active");
-    }
-
-    document.getElementById(tabName).style.display = "block";
-
-    evt.currentTarget.classList.add("active");
-
-    evt.preventDefault();
-}
-
-document.getElementById("tab1").style.display = "block";
-document.getElementsByClassName("tablinks")[0].classList.add("active");
-
-
 function openModal(imgSrc, title, description) {
     var modal = document.getElementById("myModal");
     var modalImg = document.getElementById("modalImg");
